7cce998bbb1d6892a22c8c1d46557ed0c1d3eed0
Matthew Golovanov <matt.golovanov@gmail.com>
2025-11-09 16:39:26 -0800

    Added sfx

--- ball_physics.gd
+++ ball_physics.gd
@@ -11,5 +11,18 @@ func _integrate_forces(state):
 		if(state.get_contact_collider_object(0).get_parent().has_meta(&"type") && state.get_contact_collider_object(0).get_parent().get_meta(&"type") == "Bumper"):
 			#print(state.get_contact_local_normal(0))
 			state.apply_impulse(state.get_contact_local_normal(0) * bumperBounciness, state.get_contact_collider_position(0))
+			$BumperNoise.play()
 		else:
 			state.apply_impulse(state.get_contact_local_normal(0) * defaultBounciness, state.get_contact_collider_position(0))
+			$RandomNoisePlayer.play()
+
+func _ready():
+	$VisibleOnScreenNotifier2D.screen_exited.connect(handle_death)
+
+func handle_death():
+	freeze = true
+	$DeathNoise.play()
+	await get_tree().create_timer(1.0).timeout
+	
+	queue_free()
+	
